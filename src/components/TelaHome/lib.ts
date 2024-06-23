42bf336bd152098d9ade250b8d4aa9dc8f4fe0cc
Junior <juniorwgalhardo@aluno.santoangelo.uri.br>
2024-06-23 11:37:04 -0300

    ✨ Menu lateral

---
+++ src/components/TelaHome/lib.ts
@@ -0,0 +1,6 @@
+export const routers = [
+  { icon: 'group', name: 'lista de moradores' },
+  { icon: 'location_on', name: 'alugar salao' },
+  { icon: 'calendar_view_month', name: 'mural de recados' },
+  { icon: 'paid', name: 'relatorio de gastos' }
+]
