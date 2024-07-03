8e8249c145a3531e7f4a466879fcf5eaccf90ae4
oGalhardo <juniorgalhardo03@gmail.com>
2024-07-03 17:55:28 -0300

    Adicionando side menu e tab header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
-<script setup lang="ts"></script>
-
 <template>
-  <RouterView />
+  <q-toolbar class="text-primary bg-primary z-top">
+    <q-btn flat round dense icon="menu" color="white" @click="openList = !openList" />
+    <q-toolbar-title class="text-white"> Menu </q-toolbar-title>
+  </q-toolbar>
+  <q-layout class="q-py-md">
+    <q-drawer v-model="openList" show-if-above elevated class="bg-primary text-white">
+      <q-scroll-area class="fit">
+        <q-item v-for="rout in routers" clickable @click="changeRout(rout.name)">
+          <q-item-section avatar>
+            <q-icon :name="rout.icon" color="white" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-capitalize">{{ rout.name }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const routers = [
+  { icon: 'group', name: 'lista de moradores' },
+  { icon: 'location_on', name: 'agendador' },
+  { icon: 'calendar_view_month', name: 'mural de recados' },
+  { icon: 'paid', name: 'relatorio de gastos' },
+  { icon: 'edit', name: 'permissoes' }
+]
+const router = useRouter()
+function changeRout(rout: string) {
+  router.push(`/${rout}`)
+}
+const openList = ref(false)
+</script>
--- src/components/TelaHome/TelaAgendador/CalendarSalao.vue
+++ src/components/TelaHome/TelaAgendador/CalendarSalao.vue
@@ -1,22 +1,37 @@
 <template>
-  <MontVisualization :select-date="selectedDate" />
-  <MonthNavigation @today="onToday" @prev="onPrev" @next="onNext" />
-  <div class="row justify-center font-custom">
-    <div class="text-h5 calendar-size text-uppercase">
-      <q-btn label="Agendar Evento" text-color="white" color="orange" @click="showCard = true" />
-      <ModalAgendarAdd :show-card="showCard" @close="showCard = false" @salva-agend="handleDates" />
-      <q-calendar-month
-        ref="calendar"
-        v-model="selectedDate"
-        locale="pt-br"
-        :day-min-height="100"
-        class="cursor-pointer"
-      >
-        <template #day="{ scope: { timestamp } }">
-          <BadgeEvents :data="timestamp.date" />
-        </template>
-      </q-calendar-month>
-    </div>
+  <div class="q-ma-md">
+    <DrawherMenu :open-list="true">
+      <ContentSection title-align="center">
+        <template #title>{{ formattedMonth }} </template>
+        <MonthNavigation @today="onToday" @prev="onPrev" @next="onNext" />
+        <div class="row justify-center font-custom">
+          <div class="text-h5 calendar-size text-uppercase">
+            <q-btn
+              label="Agendar Evento"
+              text-color="white"
+              color="orange"
+              @click="showCard = true"
+            />
+            <ModalAgendarAdd
+              :show-card="showCard"
+              @close="showCard = false"
+              @salva-agend="handleDates"
+            />
+            <q-calendar-month
+              ref="calendar"
+              v-model="selectedDate"
+              locale="pt-br"
+              :day-min-height="100"
+              class="cursor-pointer"
+            >
+              <template #day="{ scope: { timestamp } }">
+                <BadgeEvents :data="timestamp.date" />
+              </template>
+            </q-calendar-month>
+          </div>
+        </div>
+      </ContentSection>
+    </DrawherMenu>
   </div>
 </template>
 
@@ -27,6 +42,8 @@ import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarMonth.sass'
 import { useEventStore } from '../../../stores/event'
+import { countryCodes } from '../lib'
+
 const eventStorage = useEventStore()
 const selectedDate = ref(today())
 const instance = getCurrentInstance()
@@ -55,6 +72,29 @@ function handleDates(val: any, val2: any, val3?: any) {
   notifyPositive('Agendado com sucesso!')
   return (showCard.value = false)
 }
+
+const formattedMonth = computed(() => {
+  const date = new Date(selectedDate.value)
+  return monthFormatter().format(date) + ' ' + date.getFullYear()
+})
+const country = ref('BR')
+function monthFormatter(): Intl.DateTimeFormat {
+  try {
+    return new Intl.DateTimeFormat(locale.value || undefined, {
+      month: 'long',
+      timeZone: 'UTC'
+    })
+  } catch (e) {
+    console.error('Failed to create DateTimeFormat:', e)
+    return new Intl.DateTimeFormat('en-US', { month: 'long' })
+  }
+}
+const locale = computed(() => {
+  if (country.value) {
+    return countryCodes[country.value]
+  }
+  return 'pt-BR'
+})
 </script>
 
 <style scoped>
